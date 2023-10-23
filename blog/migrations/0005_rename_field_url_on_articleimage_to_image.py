29702beb20620a639b50b98cbe31a2981065787a
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-10-23 16:23:36 +0330

    style: Rename migration files

--- blog/migrations/0005_rename_field_url_on_articleimage_to_image.py
+++ blog/migrations/0005_rename_field_url_on_articleimage_to_image.py
@@ -5,7 +5,7 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("blog", "0004_articleimage"),
+        ("blog", "0004_create_articleimage"),
     ]
 
     operations = [
