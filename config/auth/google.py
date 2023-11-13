baadddb1a2884017aac9287bb7e65fb181946660
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-11-13 23:08:01 +0330

    chore: Add python-dotenv package

--- config/auth/google.py
+++ config/auth/google.py
@@ -1,4 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 GOOGLE_OAUTH2_CLIENT_ID = os.environ.get("DJANGO_GOOGLE_OAUTH2_CLIENT_ID")
 GOOGLE_OAUTH2_CLIENT_SECRET = os.environ.get("DJANGO_GOOGLE_OAUTH2_CLIENT_SECRET")
