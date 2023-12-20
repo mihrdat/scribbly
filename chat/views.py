767401dc2512f66b05839af1b8c78e6c247d6363
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-12-20 14:53:44 +0330

    feat: Secure rooms and messages API endpoints

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 from .serializers import RoomSerializers, MessageSerializers
 from .models import Room, Message
@@ -9,6 +10,7 @@ from .models import Room, Message
 class RoomViewSet(ReadOnlyModelViewSet):
     queryset = Room.objects.select_related("partner").all()
     serializer_class = RoomSerializers
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return super().get_queryset().filter(user=self.request.user)
@@ -17,6 +19,7 @@ class RoomViewSet(ReadOnlyModelViewSet):
 class MessageViewSet(ReadOnlyModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializers
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         room = get_object_or_404(Room, pk=self.kwargs["room_pk"])
