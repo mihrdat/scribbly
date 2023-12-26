8069c97eeb737bc9e113f656b5a8a9adb2031bb4
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-12-26 16:58:58 +0330

    feat: Set default value for name field

--- chat/models.py
+++ chat/models.py
@@ -3,12 +3,14 @@ import uuid
 from django.db import models
 from django.contrib.auth import get_user_model
 
+from .utils import generate_random_room_name
+
 User = get_user_model()
 
 
 class Room(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=55)
+    name = models.CharField(max_length=55, default=generate_random_room_name)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="rooms")
     partner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="+")
 
