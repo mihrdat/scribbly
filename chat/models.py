f9945da5bfeeaa67a842347ead7aad9385179ae4
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2024-03-29 14:48:51 +0330

    feat: Create generate_random_room_name function

--- chat/models.py
+++ chat/models.py
@@ -4,15 +4,19 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class Room(models.Model):
+class Chat(models.Model):
     name = models.CharField(max_length=55)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="rooms")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="chats")
     participant = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Message(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="messages")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="sent_messages"
+    )
+    recipient = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="received_messages"
+    )
     created_at = models.DateTimeField(auto_now_add=True)
