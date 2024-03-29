27f670baad7efd52aed073ff59d0288de8b7c6d8
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2024-03-29 14:29:08 +0330

    feat: Create Room and Message models in chat app

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=55)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="rooms")
+    participant = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="messages")
+    created_at = models.DateTimeField(auto_now_add=True)
