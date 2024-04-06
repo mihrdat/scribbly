8ad60834d4200036770b49124480b60af89f14f9
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2024-04-06 10:09:52 +0330

    refactor: Remove messages API endpoint

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,4 +1,5 @@
 import json
+
 from django.db.models import Q
 from django.contrib.auth import get_user_model
 from asgiref.sync import async_to_sync
@@ -15,94 +16,99 @@ class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
 
-        if not self.authenticate_user():
+        user = self.scope["user"]
+        if not user.is_authenticated:
+            detail = {
+                "type": "error",
+                "message": Messages.ERROR_AUTHENTICATION_FAILED,
+            }
+            self.send(json.dumps(detail))
+            self.close()
             return
 
-        self.assign_participant()
-        self.assign_room()
+        participant_id = self.scope["url_route"]["kwargs"]["participant_id"]
+        if participant_id == 0:
+            self.participant = self.get_random_admin()
+        else:
+            try:
+                self.participant = self.get_participant(participant_id)
+            except User.DoesNotExist:
+                detail = {
+                    "type": "error",
+                    "message": Messages.ERROR_NO_USER_FOUND,
+                }
+                self.send(json.dumps(detail))
+                self.close()
+                return
+
+        self.room_group_name = self.get_shared_room_name(user, self.participant)
 
         async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name,
-            self.channel_name,
+            self.room_group_name, self.channel_name
         )
 
-        self.send(
-            json.dumps(
+        detail = {
+            "type": "connection_established",
+            "message": Messages.CONNECTION_SUCCESS_MESSAGE,
+            "history": [
                 {
-                    "type": "connection_established",
-                    "detail": Messages.CONNECTION_SUCCESS_MESSAGE,
+                    "sender_id": message.sender_id,
+                    "content": message.content,
+                    "created_at": message.created_at.isoformat(),
                 }
-            )
-        )
+                for message in self.get_history(user, self.participant)
+            ],
+        }
+        self.send(json.dumps(detail))
 
     def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
         async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat_message", "detail": message}
+            self.room_group_name, {"type": "chat_message", "message": message}
         )
 
         user = self.scope["user"]
-        Room.objects.get_or_create(
-            user=user,
-            participant=self.participant,
-            defaults={"name": self.room_group_name},
-        )
-        Room.objects.get_or_create(
-            user=self.participant,
-            participant=user,
-            defaults={"name": self.room_group_name},
-        )
+        self.create_room_with_shared_name(user, self.participant, self.room_group_name)
+        self.create_room_with_shared_name(self.participant, user, self.room_group_name)
 
-        Message.objects.create(content=message, user=user, recipient=self.participant)
+        Message.objects.create(content=message, sender=user, recipient=self.participant)
 
     def chat_message(self, event):
-        message = event["message"]
-        self.send(json.dumps({"type": "chat", "detail": message}))
-
-    def authenticate_user(self):
-        user = self.scope["user"]
-        if not user.is_authenticated:
-            self.send(
-                json.dumps(
-                    {"type": "error", "detail": Messages.ERROR_AUTHENTICATION_FAILED}
-                )
-            )
-            self.close()
-            return False
-        return True
-
-    def assign_participant(self):
-        username = self.scope["url_route"]["kwargs"]["username"]
-        self.participant = User.objects.filter(username=username).first()
-        if (username != "support") and (not self.participant):
-            self.send(
-                json.dumps({"type": "error", "detail": Messages.ERROR_NO_USER_FOUND})
-            )
-            self.close()
-            return
-        elif username == "support":
-            self.participant = self.get_random_admin()
-
-    def assign_room(self):
-        user = self.scope["user"]
-        room = Room.objects.filter(
-            (Q(user=user) & Q(participant=self.participant))
-            | (Q(user=self.participant) & Q(participant=user))
-        ).first()
-        self.room_group_name = room.name if room else generate_random_room_name()
+        detail = {
+            "type": "chat",
+            "message": event["message"],
+        }
+        self.send(json.dumps(detail))
 
     def get_random_admin(self):
         return User.objects.filter(is_staff=True).order_by("?").first()
 
-    def get_messages(self, user, participant):
-        return [
-            (message.user.username, message.content)
-            for message in Message.objects.select_related("user")
-            .filter(
-                Q(user=user, recipient=participant)
-                | Q(user=participant, recipient=user)
+    def get_participant(self, pk):
+        return User.objects.get(pk=pk)
+
+    def get_shared_room_name(self, user, participant):
+        room = Room.objects.filter(
+            Q(user=user, participant=participant)
+            | Q(user=participant, participant=user)
+        ).first()
+        return room.name if room else generate_random_room_name()
+
+    def get_history(self, user, participant):
+        try:
+            room = Room.objects.get(user=user, participant=participant)
+            return (
+                Message.objects.filter(
+                    Q(sender=user, recipient=participant)
+                    | Q(sender=participant, recipient=user)
+                )
+                .filter(created_at__gte=room.created_at)
+                .order_by("created_at")
             )
-            .order_by("created_at")
-        ]
+        except Room.DoesNotExist:
+            return []
+
+    def create_room_with_shared_name(self, user, participant, name):
+        if not Room.objects.filter(user=user, participant=participant).exists():
+            Room.objects.create(name=name, user=user, participant=participant)
